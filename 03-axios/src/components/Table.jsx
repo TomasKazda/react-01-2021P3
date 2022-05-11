0ea095ae29992eb1bcce889a924d18426a05cd3f
Tomáš Kazda <tomas.kazda@pslib.cz>
2022-05-11 14:13:23 +0200

    Object.entries map

--- 03-axios/src/components/Table.jsx
+++ 03-axios/src/components/Table.jsx
@@ -1,24 +1,24 @@
 
 const Table = ({tabledata, ...rest}) => {
-    console.log(tabledata)
     const dateval = tabledata === {} ? "Žádná data" : tabledata.date
 
-    const header = []
-    const rows = []
+    let header = []
+    let rows = []
     if (Array.isArray(tabledata.items)) {
-        for (const y in tabledata.items[0]) {
-            header.push(<th>{y}</th>)
-        }
-        for (const x of tabledata.items)
-        {
-            rows.push(<tr>
-                <td>{x['zeme']}</td>
-                <td>{x['mena']}</td>
-                <td>{x['mnozstvi']}</td>
-                <td>{x['kod']}</td>
-                <td>{x['kurz']}</td>
-            </tr>)
-        }
+        header = Object.entries(tabledata.items[0]).map(
+            ([k,v],i) => 
+                <th key={i}>{k}</th>
+            )
+
+        rows = tabledata.items.map(
+            (v,i) => 
+                <tr key={i}>{
+                    Object.entries(v).map(
+                        ([key,val],j) => 
+                            <td key={j}>{val}</td>
+                    )
+                }</tr>
+            )
     }
     
     return (
